6a0f3546a961873b2da6b3f8be8e0d24a55dcdc5
Pedro Lucena <pedro1lucena@hotmail.com>
2021-10-25 17:01:00 -0300

    atualização no arquivo JS

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ function changeSocialMediaLinks() {
     for(let li of socialLinks.children) {
         const social = li.getAttribute('class')         
           li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
-          alert(li.children[0].href)
     }
 }
       changeSocialMediaLinks()
