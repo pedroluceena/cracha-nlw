94de55e01e4aae028d72c6ef7dae41a7fe4f8dc3
Pedro Lucena <pedro1lucena@hotmail.com>
2021-10-25 17:04:26 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,8 +12,7 @@ function changeSocialMediaLinks() {
           li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
     }
 }
-      changeSocialMediaLinks()
-
+      
 function getGitHubProfileInfos (){
     const url = `https://api.github.com/users/${LinksSocialMedia.github}`
   
