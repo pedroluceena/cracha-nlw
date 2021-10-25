13d20096fc91f05966cee7503037964baa9419a6
Pedro Lucena <pedro1lucena@hotmail.com>
2021-10-24 21:32:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const LinksSocialMedia = {
+github: "pedroluceena",
+youtube: "UC51TNDe_bMTNpcWcESV3L_A",
+facebook: "pedro.lucena.543",
+instagram: "pelucenap",
+twitter: "pelucenap"
+}
+      
+function changeSocialMediaLinks() {
+    for(let li of socialLinks.children) {
+        const social = li.getAttribute('class')         
+          li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+          alert(li.children[0].href)
+    }
+}
+      changeSocialMediaLinks()
+
+function getGitHubProfileInfos (){
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio 
+      userImage.src = data.avatar_url
+    })
+}
+
+getGitHubProfileInfos ()
